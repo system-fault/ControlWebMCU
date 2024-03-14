a6e0d31aad95a82271a83a58ea6f9f7885be4ed2
system-fault <thpuppetmaster@gmail.com>
2024-03-14 22:25:08 +0100

    commit: http funcional

--- components/custom_HTTP_client/include/custom_HTTP_client.h
+++ components/custom_HTTP_client/include/custom_HTTP_client.h
@@ -30,19 +30,40 @@
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "esp_err.h"
+#include "sensorbmp280.h"
+#include "driver/gpio.h"
+#include "init_gpio.h"
 
 #ifndef DELAY_LED_REQUEST
 #define DELAY_LED_REQUEST 200
 #endif
 
+#ifndef DELAY_POST_DATA
+#define DELAY_POST_DATA 5000
+#endif
+
+
+extern datosSensor http_medidas_var;
+
 
 
 #endif /* CUSTOM_HTTTP_CLIENT_H */
 
+//$ FUNCIONES PARA MANEJAR LAS PETICIONES GET AL SERVIDOR WEB //
 
-
-esp_err_t client_event_get_handler(esp_http_client_event_handle_t evt);
+esp_err_t client_event_get_handler_temp(esp_http_client_event_handle_t evt);
+esp_err_t client_event_get_handler_pres(esp_http_client_event_handle_t evt);
 
 void lectura_estado_led_Task(void *pvParameters);
 
 esp_err_t actualizar_led(int *estado);
+
+
+//& FUNCIONES PARA MANEJAR LOS ENVIOS POST AL SERVIDOR WEB //
+
+
+esp_err_t adq_medidas(datosSensor datos);
+
+esp_err_t client_event_post_handler(esp_http_client_event_handle_t evt);
+
+void envio_datos_post_Task(void *pvParameters);
--- main/main.c
+++ main/main.c
@@ -8,8 +8,6 @@
 #include "init_custom_wifi.h"
 #include "custom_HTTP_client.h"
 
-
-
 #define DELAY 250
 
 static const char *TAG_MAIN = "MAIN";
@@ -19,6 +17,8 @@ int nivel_led;
 
 datosSensor datos;
 
+void mainTask(void *pvParameters);
+
 void app_main(void)
 {
     ESP_ERROR_CHECK(wifi_launch());
@@ -26,15 +26,26 @@ void app_main(void)
     ESP_ERROR_CHECK(init_led());
     xTaskCreatePinnedToCore(bmp280_Task, "bmp280_Task", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL, APP_CPU_NUM);
     xTaskCreatePinnedToCore(lectura_estado_led_Task, "lectura_estado_led_Task", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL, APP_CPU_NUM);
+    xTaskCreatePinnedToCore(envio_datos_post_Task, "envio_datos_post_Task", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL, APP_CPU_NUM);
+    xTaskCreatePinnedToCore(mainTask, "mainTask", configMINIMAL_STACK_SIZE * 8, NULL, 5, NULL, PRO_CPU_NUM);
+    
+}
+
+
+//? TAREA PRINCIPAL(MAIN)
+void mainTask(void *pvParameters)
+{
 
     while (1)
     {
         ESP_ERROR_CHECK(actualizarDatos(&datos));
         temperatura = datos.temperaturaMain;
         presionBar = datos.presionMain;
-        ESP_LOGI(TAG_MAIN,"Presion: %.2f Bar, Temperatura: %.2f ºC",presionBar,temperatura);
+        ESP_LOGI(TAG_MAIN, "Presion: %.2f Bar, Temperatura: %.2f ºC", presionBar, temperatura);
         ESP_ERROR_CHECK(actualizar_led(&nivel_led));
-        gpio_set_level(LED_PIN_G,nivel_led);
+        ESP_ERROR_CHECK(adq_medidas(datos));
+        gpio_set_level(LED_PIN_G, nivel_led);
+
         vTaskDelay(pdMS_TO_TICKS(DELAY));
     }
 }
